7da6a5c932244e8eb56a0019ea8d5446bfb6445e
Behrooze Sirang <behrooze@gmail.com>
2021-06-01 21:13:10 -0700

    Access garmin decode module from container

--- docker/run.sh
+++ docker/run.sh
@@ -13,6 +13,7 @@ docker run \
   -it \
   -v "$SCRIPT_DIR/$NOTEBOOKS_DIR":/home/jovyan/work \
   -v "$SCRIPT_DIR/$DATA_DIR":/home/jovyan/data \
+  -v "$SCRIPT_DIR/../garmin_decode":/home/jovyan/garmin_decode \
+  --env PYTHONPATH=/home/joyvan \
   -p 8888:8888 \
   "$DOCKER_IMAGE_NAME" $@
-
